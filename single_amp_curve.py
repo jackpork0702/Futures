8a21b6499d4014718b52a5e3cf2fdc927fdbb152
Berchem <34000467+Berchem@users.noreply.github.com>
2018-09-10 14:46:56 +0800

    beta0.1.1

--- single_amp_curve.py
+++ single_amp_curve.py
@@ -1,7 +1,5 @@
-from datetime import datetime
 import matplotlib.pyplot as plt
 from load_database import Load
-from MypseudoSQL import Table
 
 """
 use tick_9_2.db
@@ -25,9 +23,6 @@ raw_close = db.get_per_min(start="2017-01-01", drop_night_trade=True, predicate=
 
 columns = ("Date", "Time", "Open", "High", "Low", "Close", "TotalVolume")
 data = map(lambda record: dict(zip(columns, record)), raw_data)  # a map generator, dict of list
-# close = map(lambda record: dict(zip(columns, record)), raw_close)  # a map generator, dict of list
-
-# data = [dict(zip(columns, record)) for record in raw_data]
 close = [dict(zip(columns, record)) for record in raw_close]
 
 
@@ -63,23 +58,3 @@ plt.plot(ma_5_5, '-')
 plt.plot(ma_35_5, '-')
 plt.plot(ma_200_5, '-')
 plt.show()
-# trad_day = Table(["Date", "Time", "Open", "High", "Low", "Close", "TotalVolume"])
-#
-# for record in trading_day.rows:
-#     if record["Date"] not in map(lambda row: row["Date"], trading_day.rows):
-#         trad_day.insert(record)
-#
-# print("data length:", len(table.rows))
-# print("close length:", len(close_13_45.rows))
-# print("trading days:", len(trad_day.rows))
-
-# for key, val in table.rows[-1].items():
-#     print(key, type(val))
-
-# time = [datetime.strptime("T".join(record[:2]), "%Y/%m/%dT%H:%M:%S")
-#         for record in data_per_min]
-#
-# print("since 2017 (day and night):", len(data_per_min))
-# print("every close price (day trade):", len(every_close))
-
-
