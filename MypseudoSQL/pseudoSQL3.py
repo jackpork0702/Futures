86a39736effc720f4d9ba1de3aeca24122f3cab7
Berchem <34000467+Berchem@users.noreply.github.com>
2018-09-20 13:49:14 +0800

    v0.1.1

--- MypseudoSQL/pseudoSQL3.py
+++ MypseudoSQL/pseudoSQL3.py
@@ -110,7 +110,7 @@ class Table:
         :return: table
         """
         where_table = Table(self.columns)
-        where_table.rows = [filtered for filtered in filter(predicate, self.rows)]
+        where_table.rows = list(filter(predicate, self.rows))
         return where_table
 
     def group_by(self, group_by_columns, aggregates, having=None):
@@ -120,7 +120,7 @@ class Table:
             key = tuple(row[column] for column in group_by_columns)
             grouped_rows[key] += [row]
 
-        result_table = Table(group_by_columns + aggregates.keys())
+        result_table = Table(group_by_columns + list(aggregates.keys()))
 
         for key, rows in grouped_rows.items():
             if having is None or having(rows):
@@ -128,7 +128,8 @@ class Table:
 
                 for aggregate_name, aggregate_fn in aggregates.items():
                     new_row += [aggregate_fn(rows)]
-                    result_table.insert(new_row)
+                    
+                result_table.insert(new_row)
 
         return result_table
 
